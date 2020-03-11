9801af5852cdbc27289887390fb2462c84674365
Enrico Massaro <emassaro98@users.noreply.github.com>
2020-03-11 17:10:56 +0100

    Modifiche grafiche
    - messa l ombra alla navbar
    
    P.s Posa ingoia

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -8,16 +8,15 @@ use View\Render;
 class HomeController {
 
     public function index() {
-        /*
-            Esempio di utilizzo
-            
+
+            // Esempio di utilizzo
             $contact = Contact::find(['id' => 1]);
-            $contact->setName("ProvaName");
-            $contact = $contact->save();
-            
+            //$contact->setName("ProvaName");
+            //$contact->setEmail("ProvaName");
+            //$contact->setMessage("ProvaName");
+            //$contact = $contact->save();
             print_r($contact);
-        */
-        
+
         return Render::index("Home", []);
     }
 }
