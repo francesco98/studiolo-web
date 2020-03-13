b7de391afc561965ee7abe57ea38323a916be807
Francesco <posa.francesco98@gmail.com>
2020-03-13 17:17:26 +0100

    blog updated

--- View/template/blog_old.php
+++ View/template/blog_old.php
@@ -1,43 +1,45 @@
 <?php
-$utilityObject->fixedBar=true;
+$utilityObject->fixedBar = true;
+$primoArticolo = array_pop($object->articles);
 ?>
 <section class="section-blog">
     <div class="container">
         <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark gradient-post">
             <div class="col-md-6 px-0">
-                <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-                <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
-                    efficiently about what’s most interesting in this post’s contents.</p>
-                <p class="lead mb-0"><a href="#" class="text-yellow font-weight-bold">Continue reading...</a></p>
+                <h1 class="display-4 font-italic"><?=$primoArticolo->getTitle()?></h1>
+                <p class="lead my-3">
+                    <?php
+                    if(strlen($primoArticolo->getText()) > 100) {
+                        echo substr($primoArticolo->getText(), 0, 100) . "...";
+                    } else {
+                        echo $primoArticolo->getText();
+                    }
+                    ?>
+                </p>
+                <p class="lead mb-0"><a href="#" class="text-yellow font-weight-bold">Leggi di pi&ugrave;...</a></p>
             </div>
         </div>
-        <div class="row mb-2">
-            <div class="col-md-6">
-                <div
-                    class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary">World</strong>
-                        <h3 class="mb-0">Featured post</h3>
-                        <div class="mb-1 text-muted">Nov 12</div>
-                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
-                            lead-in to additional content.</p>
-                        <a href="#" class="stretched-link text-yellow">Continue reading</a>
+
+        <?php
+        foreach ($object->articles as $articolo) {
+        ?>
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 class="mb-0">Featured post</h3>
+                            <div class="mb-1 text-muted">Nov 12</div>
+                            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                            <a href="#" class="stretched-link text-yellow">Continue reading</a>
+                        </div>
                     </div>
                 </div>
+
+            <?php
+        }
+            ?>
             </div>
-            <div class="col-md-6">
-                <div
-                    class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-muted">Nov 11</div>
-                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to
-                            additional content.</p>
-                        <a href="#" class="stretched-link text-yellow">Continue reading</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </section>
