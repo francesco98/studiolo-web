d98d8aac983835279815172d34050820015fe74b
vanessatamma <vanessatamma@users.noreply.github.com>
2020-03-13 19:17:08 +0100

    lista articoli
    posa canta i canti gregoriani

---
+++ View/template/listArticle.php
@@ -0,0 +1,29 @@
+<?php
+$utilityObject->fixedBar=true;
+?>
+<section class="masthead">
+    <div class="container">
+        <div class="card p-5 shadow-sm rounded">
+            <div class="row mb-5 d-block p-4">
+                <a href="#" class="btn btn-warning float-right">Nuovo articolo</a>
+            </div>
+            <div class="row border-bottom">
+                <div class="col-8">
+                    <h5>
+                        <strong>
+                            Titolo
+                        </strong>
+                    </h5>
+                    <p class="text-monospace text-grey">10 Ottobre 2019</p>
+                </div>
+                <div class="col-2 text-right p-0">
+                    <a href="#" class="btn btn-link text-blue">Modifica</a>
+                </div>
+                <div class="col-2 p-0">
+                    <a href="#" class="btn btn-link text-green">Elimina</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
